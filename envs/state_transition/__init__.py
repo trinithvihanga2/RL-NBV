e9497a1653712a40e435eaeeb0c1af2bac970409
Yumeth <yumeth@entgra.io>
2026-06-08 10:52:20 +0530

    Remove unused imports and unused

--- envs/state_transition/__init__.py
+++ envs/state_transition/__init__.py
@@ -1,9 +1,8 @@
-"""State transition module for orbital/target-based travel time calculations."""
+"""State transition module for continuous orbital environment updates."""
 
-from .coverage import update_coverage_map, CoverageUpdateResult
+from .coverage import update_continuous_coverage
 from .cw_utils import compute_delta_v_matrix
-from .reward import calculate_reward, RewardResult
-from .state_builder import build_state, State
+from .reward import calculate_continuous_reward
 from .sun_position import calculate_sun_position
 from .travel_time import (
     TargetOrbitConfig,
@@ -13,7 +12,7 @@ from .travel_time import (
     advance_time,
     compute_all_travel_times,
 )
-from .visibility import get_lit_visible_points
+from .visibility import filter_lit_points
 
 __all__ = [
     "TargetOrbitConfig",
@@ -23,12 +22,8 @@ __all__ = [
     "advance_time",
     "compute_all_travel_times",
     "calculate_sun_position",
-    "get_lit_visible_points",
-    "update_coverage_map",
-    "CoverageUpdateResult",
-    "calculate_reward",
-    "RewardResult",
-    "build_state",
-    "State",
+    "filter_lit_points",
+    "update_continuous_coverage",
+    "calculate_continuous_reward",
     "compute_delta_v_matrix",
 ]
